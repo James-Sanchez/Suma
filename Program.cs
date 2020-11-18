b3f8ff46f7c773613fc6bdc01aec3cf1683f3ee2
James Sánchez <1the1.1key1@gmail.com>
2020-11-17 23:26:22 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+	class Program
+	{
+		static void Integers()
+		{
+			int numero1 = 137;
+			int numero2 = 63;
+			int resultado = numero1 + numero2;
+
+			Console.WriteLine("El resultado es: " + resultado);
+
+			Console.ReadKey();
+		}
+		static void Main(string[] args)
+		{
+			Integers();
+		}
+	}
+}
