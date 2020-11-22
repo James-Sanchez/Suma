84ccc2543c2ef8fefb01a9007d1523fa7e2a1d35
SteveSanchz <steven.nathan.sanchez@outlook.com>
2020-11-22 05:30:16 -0600

    Se creo una clase especial y sus correspondientes metodos para validar entradas

---
+++ Herramienta.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma
+{
+    class Herramienta
+    {
+        private string entrada1;
+        private string entrada2;
+        private string mensaje1;
+        private string mensaje2;
+		private string mensajeAdvertencia = "No puedes introducir ningún otro caracter.Solo números.";
+        private float n1;
+        private float n2;
+        private float resultado;
+
+        public void setMensajes(string primermensaje, string segundomensaje)
+        {
+            this.mensaje1 = primermensaje;
+            this.mensaje2 = segundomensaje;
+        }
+        public void realizarSuma()// Devuelve verdadero si el dato ingresado es un numero
+        {
+			while (true)
+			{
+				Console.WriteLine(mensaje1);
+				entrada1 = Console.ReadLine();
+				if (float.TryParse(entrada1,out n1))
+				{
+					break;
+				}
+				else
+				{
+					Console.WriteLine(mensajeAdvertencia);
+				}
+			}
+
+			while (true)
+			{
+				Console.WriteLine(mensaje2);
+				entrada2 = Console.ReadLine();
+				if (float.TryParse(entrada2, out n2))
+				{
+					break;
+				}
+				else
+				{
+					Console.WriteLine(mensajeAdvertencia);
+				}
+			}
+			resultado = n1 + n2;
+		}
+        public void imprimirResultado()
+        {
+            Console.WriteLine("\nEl resultado es: " + resultado + "\nPresiona cualquier tecla para finalizar el programa");
+			Console.ReadKey(true);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,51 +8,12 @@ namespace Suma
 {
 	class Program
 	{
-		static void Integers()
-		{
-
-			string entrada1, entrada2;
-			float resultado, n1, n2;
-
-
-			while (true)
-			{
-				Console.WriteLine("Ingresa el primer número: ");
-				entrada1 = Console.ReadLine();
-				if (float.TryParse(entrada1, out n1))
-				{
-					//n1 = entrada1;
-					break;
-				}
-				else
-				{
-					Console.WriteLine("No puedes introducir ningún otro caracter. Solo números.");
-				}
-			}
-
-			while (true)
-			{
-				Console.WriteLine("Ingresa el segundo número: ");
-				entrada2 = Console.ReadLine();
-				if (float.TryParse(entrada2, out n2))
-				{
-					//n2 = entrada2;
-					break;
-				}
-				else
-				{
-					Console.WriteLine("No puedes introducir ningún otro caracter. Solo números.");
-				}
-			}
-
-			resultado = n1 + n2;
-			Console.WriteLine("El resultado es: " + resultado);
-
-			Console.ReadKey();
-		}
 		static void Main(string[] args)
 		{
-			Integers();
+			Herramienta herramienta = new Herramienta();
+			herramienta.setMensajes("Introduce el primer numero", "Introduce el segundo numero");
+			herramienta.realizarSuma();
+			herramienta.imprimirResultado();
 		}
 	}
 }
